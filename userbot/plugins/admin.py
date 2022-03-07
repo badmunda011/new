646eb0911ad6ffe1c6bb70a97f592b41fa2e95f0
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-03-08 00:26:07 +0530

    Update admin.py

--- userbot/plugins/admin.py
+++ userbot/plugins/admin.py
@@ -745,10 +745,10 @@ async def _iundlt(event):  # sourcery no-metrics
             _media_type = media_type(msg.old)
             if _media_type is None:
                 await main_msg.reply(
-                    f"✓ {_format.mentionuser(sweet.first_name ,sweet.id)} :\n __{msg.old.message}__"
+                    f"✓ {_format.mentionuser(sweet.first_name ,sweet.id)} : __{msg.old.message}__"
                 )
             else:
                 await main_msg.reply(
-                    f"✓ {_format.mentionuser(sweet.first_name ,sweet.id)} \n __{msg.old.message}__",
+                    f"✓ {_format.mentionuser(sweet.first_name ,sweet.id)} : __{msg.old.message}__",
                     file=msg.old.media,
                 )
