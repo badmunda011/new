39cc87f03b13065ecb6575a5c1eb4bb18492c2b3
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-03-08 01:38:45 +0530

    Update corecmds.py

--- userbot/plugins/corecmds.py
+++ userbot/plugins/corecmds.py
@@ -13,6 +13,7 @@ from . import (
     eor,
     legend,
     mention,
+    hmention,
     reply_id,
 )
 
@@ -21,8 +22,6 @@ from ..sql_helper.globals import gvarstatus
 
 DELETE_TIMEOUT = 5
 thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg")
-
-EVAL = gvarstatus("EVAL")
 
 
 @legend.legend_cmd(
