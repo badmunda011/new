b9271f0898c5fba8d2087c2793752e877a30bd56
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-03-15 01:59:18 +0530

    Create row.py

---
+++ userbot/plugins/row.py
@@ -0,0 +1,38 @@
+from asyncio import sleep
+
+from googletrans import LANGUAGES, Translator
+
+from userbot import legend
+
+from ..core.managers import eod, eor
+from ..sql_helper.globals import addgvar, gvarstatus
+from . import BOTLOG, BOTLOG_CHATID, deEmojify
+
+
+menu_category = "useless"
+
+@legend.legend_cmd(
+    pattern="row(?: |$)([\s\S]*)",
+    command=("row", menu_category),
+    info={
+        "header": "...",
+        "note": "..",
+        "usage": [
+            "{tr}trt <row> ; <button>",
+        ],
+    },
+)
+async def button(event):
+    input_str = event.pattern_match.group(1)
+    a = (await event.get_reply_message()).reply_markup
+    if a:
+        if ";" in input_str:
+            olo, text = input_str.split(";")
+        else:
+            await eor(event, "Check Syntax Of this cmd")
+        b = a.rows[{olo}].buttons[{text}].text
+        c = a.rows[{olo}].buttons[{text}].url
+        sweetie = f"**Text** : {b}\n**URL** :{c}"
+        await eor(event, sweetie)
+    else:
+        await eor(event, "Check information of this cmd")
