d5b3ef0bf44a7f87f1508128917fc523ed5d4926
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-03-28 20:21:20 +0530

    Update inlinebot.py

--- userbot/core/inlinebot.py
+++ userbot/core/inlinebot.py
@@ -660,6 +660,7 @@ async def inline_handler(event):  # sourcery no-metrics
             )
             await event.answer([result] if result else None)
         else:
+            await event.answer([result] if result else None)
             pass
 
 
