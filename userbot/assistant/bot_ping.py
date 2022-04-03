501cae10d014532fa39babb193a8d1969adfca18
Lêɠêɳ̃dẞογ <97836367+LEGEND-AI@users.noreply.github.com>
2022-04-04 02:48:20 +0530

    Update bot_ping.py

--- userbot/assistant/bot_ping.py
+++ userbot/assistant/bot_ping.py
@@ -9,6 +9,11 @@ from ..plugins import mention
 from ..sql_helper.bot_blacklists import check_is_black_list
 from ..sql_helper.globals import gvarstatus
 
+from ..core.logger import logging
+from ..helpers import reply_id
+from ..plugins import mention
+from ..sql_helper.bot_blacklists import check_is_black_list
+from . import BOTLOG, BOTLOG_CHATID
 LOGS = logging.getLogger(__name__)
 
 menu_category = "bot"
@@ -24,10 +29,25 @@ async def bot_start(event):
     await legend.get_me()
     if check_is_black_list(chat.id):
         return
-    GOOD = [[Button.url("⚜ Lêɠêɳ̃dẞø† ⚜", "https://t.me/LegendBot_XD")]]
+    reply_to = await reply_id(event)
+    buttons = [(Button.url("⚜ Lêɠêɳ̃dẞø† ⚜", "https://t.me/LegendBot_XD"))]
     LEGEND_IMG = gvarstatus("BOT_PING_PIC")
     start = datetime.now()
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    LegendBoy = f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{mention}』"
-    await event.client.send_file(chat.id, LEGEND_IMG, caption=LegendBoy, buttons=GOOD)
+    pm_caption = f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{mention}』"
+    try:
+        await event.client.send_file(
+            chat.id,
+            PM_IMG,
+            caption=pm_caption,
+            link_preview=False,
+            buttons=buttons,
+            reply_to=reply_to,
+        )
+    except Exception as e:
+        if BOTLOG:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                f"**Error**\nThere was a error while using **alive**. `{e}`",
+            )
