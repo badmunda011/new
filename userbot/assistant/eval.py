bae84f8c468d6801d02d82241aac6387865a51fd
Lêɠêɳ̃dẞογ <97836367+LEGEND-AI@users.noreply.github.com>
2022-04-04 01:19:26 +0530

    Create eval.py

---
+++ userbot/assistant/eval.py
@@ -0,0 +1,114 @@
+import io
+import sys
+import traceback
+
+from telethon import events
+from telethon.errors import *
+from telethon.tl.types import *
+import io
+import re
+from collections import defaultdict
+from datetime import datetime
+from typing import Optional, Union
+
+from telethon import Button, events
+from telethon.errors import UserIsBlockedError
+from telethon.events import CallbackQuery, StopPropagation
+from telethon.utils import get_display_name
+
+from userbot import Config, legend
+
+from ..core import check_owner, pool
+from ..core.logger import logging
+from ..core.session import tgbot
+from ..helpers import reply_id
+from ..helpers.utils import _format
+from ..sql_helper.bot_blacklists import check_is_black_list
+from ..sql_helper.bot_pms_sql import (
+    add_user_to_db,
+    get_user_id,
+    get_user_logging,
+    get_user_reply,
+)
+from ..sql_helper.bot_starters import add_starter_to_db, get_starter_details
+from ..sql_helper.globals import delgvar, gvarstatus
+from ..sql_helper.idaddar import get_all_users
+from . import BOTLOG, BOTLOG_CHATID
+from .botmanagers import ban_user_from_bot
+
+LOGS = logging.getLogger(__name__)
+
+menu_category = "bot"
+botusername = Config.BOT_USERNAME
+
+async def aexec(code, event):
+    exec(
+        f"async def __aexec(e, client): "
+        + "\n message = event = e"
+        + "\n reply = await event.get_reply_message()"
+        + "\n chat = (await event.get_chat()).id"
+        + "".join(f"\n {l}" for l in code.split("\n")),
+    )
+
+    return await locals()["__aexec"](event, event.client)
+  
+
+@legend.bot_cmd(
+    pattern=f"^/start({botusername})?([\s]+)?$",
+    incoming=True,
+    func=lambda e: e.sender_id == legend.uid,
+)
+async def bot_ll(event):
+    chat = await event.get_chat()
+    user = await legend.get_me()
+    if check_is_black_list(chat.id):
+        return
+    rk = await event.reply("`....`")
+    try:
+        cmd = event.text.split(" ", maxsplit=1)[1]
+    except IndexError:
+        return await rk.edit("`No Python Command Was Given`")
+    cmd = event.text.split(" ", maxsplit=1)[1]
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+
+    try:
+        await aexec(cmd, event)
+    except Exception:
+        exc = traceback.format_exc()
+
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Sᴜᴄᴄᴇss"
+    final_output = "**Eᴠᴀʟ:**\n`{}`\n\n**Oᴜᴛᴘᴜᴛ:**\n`{}`".format(cmd, evaluation)
+    MAX_MESSAGE_SIZE_LIMIT = 4095
+    if len(final_output) > MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.text"
+            await event.client.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=cmd,
+                reply_to=reply_to_id,
+            )
+
+    else:
+        await rk.edit(final_output)
