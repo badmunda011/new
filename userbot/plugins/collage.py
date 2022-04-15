56a80ad768f451a645d82469a575d391b3f02a5f
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-04-15 15:48:15 +0530

    Update collage.py

--- userbot/plugins/collage.py
+++ userbot/plugins/collage.py
@@ -10,7 +10,8 @@ import os
 from userbot import legend
 
 from ..core.managers import eod, eor
-from ..helpers import _legendutils, reply_id
+from ..helpers.utils import _legendutils
+from ..helpers import reply_id
 from . import make_gif
 
 menu_category = "utils"
