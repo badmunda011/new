77c6a74e94e2dffb35d2cb7de659ed7f2cff46af
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2022-04-15 10:18:53 +0000

    legend fixed

--- userbot/plugins/collage.py
+++ userbot/plugins/collage.py
@@ -10,8 +10,8 @@ import os
 from userbot import legend
 
 from ..core.managers import eod, eor
-from ..helpers.utils import _legendutils
 from ..helpers import reply_id
+from ..helpers.utils import _legendutils
 from . import make_gif
 
 menu_category = "utils"
