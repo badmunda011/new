0bc476f098994eb4c2ce41ac986d36be4dd4c487
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-04-17 12:37:27 +0530

    Update id.py

--- userbot/assistant/id.py
+++ userbot/assistant/id.py
@@ -17,27 +17,7 @@ botusername = Config.BOT_USERNAME
 )
 async def bot_start(event):
     "To get id of the group or user."
-    input_str = event.pattern_match.group(1)
-    if input_str:
-        try:
-            p = await event.client.get_entity(input_str)
-        except Exception as e:
-            return await event.reply(f"`{e}`", 5)
-        try:
-            if p.first_name:
-                return await event.reply(
-                    f"The id of the user `{input_str}` is `{p.id}`"
-                )
-        except Exception:
-            try:
-                if p.title:
-                    return await event.reply(
-                        f"The id of the chat/channel `{p.title}` is `{p.id}`"
-                    )
-            except Exception as e:
-                LOGS.info(str(e))
-        await event.reply("`Either give input as username or reply to user`")
-    elif event.reply_to_msg_id:
+    if event.reply_to_msg_id:
         r_msg = await event.get_reply_message()
         if r_msg.media:
             bot_api_file_id = pack_bot_file_id(r_msg.media)
