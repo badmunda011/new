29f8a7530bafa0ca764f1df036698bebd55a2a7c
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-04-26 12:37:34 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -125,7 +125,7 @@ Choose an option from below to specify the reason of your message and wait for m
     else:
         USER_BOT_NO_WARN = f"""__Hi__ {mention}__, I haven't approved you yet to personal message me.
 You have {warns}/{totalwarns} warns until you get blocked by the LegendUserBot.
-Don't spam my inbox. say reason and wait until my response.__"""
+--Don't spam my inbox. say reason and wait until my response.--"""
     addgvar("pmpermit_text", USER_BOT_NO_WARN)
     PM_WARNS[str(chat.id)] += 1
     try:
