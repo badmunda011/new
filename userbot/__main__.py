5b7e7eb3cd48dbeb74c165f2f1bb80b74951e52c
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-04-29 09:10:32 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -60,7 +60,7 @@ async def startup_process():
 legend.loop.run_until_complete(startup_process())
 legend.loop.create_task(hekp())
 
-if len(sys.argv) not in (1, 3, 4):
+if len(sys.argv) in (1, 3, 4):
     try:
         legend.run_until_disconnected()
     except ConnectionError:
