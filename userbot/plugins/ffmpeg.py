af1ddcecf24dd849fe7ecbde4f5fcc7de82e355d
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-05-03 10:48:09 +0530

    Update ffmpeg.py

--- userbot/plugins/ffmpeg.py
+++ userbot/plugins/ffmpeg.py
@@ -138,7 +138,7 @@ async def ff_mpeg_trim_cmd(event):
             end_time,
         )
         if o is None:
-            return await eod(legendevent, f"**Error : **`Can't complete the process`")
+            return await eod(legendevent, "**Error : **`Can't complete the process`")
         try:
             c_time = time.time()
             await event.client.send_file(
