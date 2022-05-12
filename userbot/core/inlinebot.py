0236c7b7738b28b2552e2c63c0cd77a1382e9bfc
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2022-05-12 16:12:33 +0000

    legend fixed

--- userbot/core/inlinebot.py
+++ userbot/core/inlinebot.py
@@ -702,6 +702,7 @@ async def inline_handler(event):  # sourcery no-metrics
             )
             await event.answer([result] if result else None)
 
+
 @legend.tgbot.on(CallbackQuery(data=re.compile(b"clise")))
 @check_owner
 async def on_plug_in_callback_query_handler(event):
