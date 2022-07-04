e1889c19ce12cb9708ea57a94d4e6a8a5da90a09
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-07-04 19:15:10 +0530

    Add files via upload

--- userbot/sql_helper/schatbot_sql.py
+++ userbot/sql_helper/schatbot_sql.py
@@ -89,12 +89,14 @@ def sremove_echo(chat_id, user_id):
 
 
 def sremove_echos(chat_id):
-    if saved_filter := SESSION.query(SEchos).filter(SEchos.chat_id == str(chat_id)):
+    saved_filter = SESSION.query(SEchos).filter(SEchos.chat_id == str(chat_id))
+    if saved_filter:
         saved_filter.delete()
         SESSION.commit()
 
 
 def sremove_all_echos():
-    if saved_filter := SESSION.query(SEchos)
+    saved_filter = SESSION.query(SEchos)
+    if saved_filter:
         saved_filter.delete()
         SESSION.commit()
