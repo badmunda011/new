dd4ecd6cd77ebc4ee67eb423d33006649eb8d2c6
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-07-04 21:05:02 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -815,12 +815,12 @@ async def pic2packcmd(event):
         except YouBlockedUserError:
             await legend(unblock("stickers"))
             await event.client.send_message(chat, "/cancel")
-            await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
-            await event.client.send_message(chat, "/newpack")
-            await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
-            await event.client.send_message(chat, args)
-            await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
-            for im in images:
+        await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
+        await event.client.send_message(chat, "/newpack")
+        await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
+        await event.client.send_message(chat, args)
+        await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
+        for im in images:
             img = io.BytesIO(im)
             img.name = name + ".png"
             img.seek(0)
