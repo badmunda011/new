be80df6f66a7bb9bc7ebf72e3826c399375df2d9
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-07-06 08:21:49 +0530

    Update dpplugin.py

--- userbot/plugins/dpplugin.py
+++ userbot/plugins/dpplugin.py
@@ -9,7 +9,24 @@ import os
 import random
 import shutil
 from datetime import datetime
+import asyncio
+import os
+import random
+import shutil
+from datetime import datetime
 
+from PIL import Image, ImageDraw, ImageFont
+from pySmartDL import SmartDL
+from telethon.tl import functions
+
+from .. import legend
+from ..core.logger import logging
+from ..core.managers import eor
+from . import mention
+
+menu_category = "useless"
+
+LOGS = logging.getLogger(__name__)
 from PIL import Image, ImageDraw, ImageFont
 from pySmartDL import SmartDL
 from telethon.tl import functions
@@ -261,3 +278,65 @@ async def autopic(event):
             await asyncio.sleep(60)
         except:
             return
+
+
+
+TELEGRAPH_MEDIA_LINKS = [
+    "https://telegra.ph/file/a2b6e3781680a6d85f842.jpg",
+    "https://telegra.ph/file/2f2035b4e8ab1dc6efc3d.jpg",
+    "https://telegra.ph/file/18e4d7c45e49ad9340e3c.jpg",
+    "https://telegra.ph/file/66205f168d8c2a0bbaa43.jpg",
+    "https://telegra.ph/file/66205f168d8c2a0bbaa43.jpg",
+    "https://telegra.ph/file/66205f168d8c2a0bbaa43.jpg",
+    "https://telegra.ph/file/3072bb5fd2c8dd8e9da60.jpg",
+    "https://telegra.ph/file/24f84ab213b177ef43d6e.jpg",
+    "https://telegra.ph/file/bc96df71964af1a4ac625.jpg",
+    "https://telegra.ph/file/bc96df71964af1a4ac625.jpg",
+    "https://telegra.ph/file/bc96df71964af1a4ac625.jpg",
+]
+
+
+@legend.legend_cmd(
+    pattern="inddp(?:\s|$)([\s\S]*)",
+    command=("inddp", menu_category),
+    info={
+        "header": "To Apply Automatically Dp ",
+        "description": "Automatically Auto Dp Apply On profile Pic",
+        "usage": [
+            "{tr}inddp",
+        ],
+    },
+)
+async def inddp(event):
+    "To Apply Automatically Dp"
+    while True:
+        piclink = random.randint(0, len(TELEGRAPH_MEDIA_LINKS) - 1)
+        AUTOPP = TELEGRAPH_MEDIA_LINKS[piclink]
+        downloaded_file_name = "./DOWNLOADS/original_pic.png"
+        downloader = SmartDL(AUTOPP, downloaded_file_name, progress_bar=True)
+        downloader.start(blocking=False)
+        photo = "photo_pfp.png"
+        while not downloader.isFinished():
+            pass
+
+        shutil.copy(downloaded_file_name, photo)
+        Image.open(photo)
+        current_time = datetime.now().strftime(
+            "\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n                                                   Time: %H:%M:%S \n                                                   Date: %d/%m/%y "
+        )
+        img = Image.open(photo)
+        drawn_text = ImageDraw.Draw(img)
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
+        drawn_text.text((300, 450), current_time, font=fnt, fill=(255, 255, 255))
+        img.save(photo)
+        file = await event.client.upload_file(photo)  # pylint:disable=E0602
+        try:
+            await event.client(
+                functions.photos.UploadProfilePhotoRequest(file)  # pylint:disable=E0602
+            )
+            os.remove(photo)
+
+            await asyncio.sleep(60)
+        except:
+            return
+
