19e09af4dd8a8f2688f82a419083eb9a1d7bed74
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-07-06 14:16:15 +0530

    Update rename.py

--- userbot/plugins/rename.py
+++ userbot/plugins/rename.py
@@ -20,7 +20,10 @@ thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg"
     command=("rnup", menu_category),
     info={
         "header": "To rename and upload the replied file.",
-        "flags": {"f": "will upload as file that is document not streamable."},
+        "flags": [
+            "f": "will upload as file that is document not streamable.",
+            "t": "will upload with thumbnail",
+        ],
         "description": "If type is not used then will upload as steamable file",
         "usage": [
             "{tr}rnup <new file name>",
