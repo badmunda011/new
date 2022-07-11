bee8b9413ed7c45081f90beeabcd47f9c9afb18d
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-07-11 14:25:19 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -24,6 +24,11 @@ print("Licensed under the terms of the " + userbot.__license__)
 
 cmdhr = Config.HANDLER
 
+async def extrarepo():
+    if Config.EXTRA_REPO:
+        await install_extrarepo(
+            Config.EXTRA_REPO, Config.EXTRA_REPOBRANCH, "xtraplugins"
+        )
 
 try:
     LOGS.info("Starting Userbot")
@@ -35,34 +40,30 @@ except Exception as e:
 
 
 async def startup_process():
-    await verifyLoggerGroup()
-    await load_plugins("plugins")
-    await load_plugins("assistant")
-    await killer()
-    print("----------------")
-    print("Starting Bot Mode!")
-    print("⚜ LegendBot Has Been Deployed Successfully ⚜")
-    print("OWNER - @LegendBoy_XD")
-    print("Group - @LegendBot_XD")
-    print("----------------")
-    await verifyLoggerGroup()
-    await add_bot_to_logger_group(BOTLOG_CHATID)
-    if PM_LOGGER_GROUP_ID != -100:
-        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
-    await startupmessage()
-    return
-
-
-async def extrarepo():
-    if Config.EXTRA_REPO:
-        await install_extrarepo(
-            Config.EXTRA_REPO, Config.EXTRA_REPOBRANCH, "xtraplugins"
-        )
+    try:
+        await verifyLoggerGroup()
+        await load_plugins("plugins")
+        await load_plugins("assistant")
+        await killer()
+        print("----------------")
+        print("Starting Bot Mode!")
+        print("⚜ LegendBot Has Been Deployed Successfully ⚜")
+        print("OWNER - @LegendBoy_XD")
+        print("Group - @LegendBot_XD")
+        print("----------------")
+        await verifyLoggerGroup()
+        await add_bot_to_logger_group(BOTLOG_CHATID)
+        if PM_LOGGER_GROUP_ID != -100:
+            await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+        await startupmessage()
+        await extrarepo()
+        await hekp()
+    except Exception as e:
+        LOGS.error(f"{str(e)}")
+        sys.exit()
 
 
 legend.loop.run_until_complete(startup_process())
-legend.loop.create_task(hekp())
-legend.loop.run_until_complete(extrarepo())
 
 if len(sys.argv) not in (1, 3, 4):
     legend.disconnect()
