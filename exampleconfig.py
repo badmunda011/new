fde91b821494f23216d0b6a1b3307674fdec1327
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-07-11 12:31:24 +0530

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -4,7 +4,7 @@ from sample_config import Config
 class Development(Config):
     # get this values from the my.telegram.org
     APP_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    API_HASH = "Your Value"
     # the name to display in your alive message
     ALIVE_NAME = "Your value"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
