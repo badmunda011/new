a50471d55eaa8a7d8708c869fba38d79393c02b1
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-07-22 00:09:22 +0530

    Delete userbot directory

--- userbot/assistant/__init__.py
+++
@@ -1,9 +0,0 @@
-from userbot import BOTLOG, BOTLOG_CHATID
-from userbot.core.session import legend, tgbot
-
-from ..Config import Config
-from ..core.inlinebot import *
-
-LEGEND_USER = legend.me.first_name
-The_LegendBoy = legend.uid
-legend_mention = f"[{LEGEND_USER}](tg://user?id={The_LegendBoy})"
